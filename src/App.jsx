fb04fc413c5b8286953271bdfccfdb77355e4f39
Colin Henderson <161981972+cochende@users.noreply.github.com>
2025-02-12 23:31:46 +0000

    restrucutre website format + minor animations(underline)

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import "./style.css";
-import Carousel from "./Carousel";
 import Typewriter from "typewriter-effect";
 
-const items = [
-  "Item 1",
-  "Item 2",
-  "Item 3"
-];
-
 const App = () => {
-  const [showProjects, setShowProjects] = useState(false);
+  const [selectedTab, setSelectedTab] = useState("intro");
+  const [fade, setFade] = useState("fade-in");
 
-  const dummy = false;
-  const toggleProjects = () => {
-    setShowProjects(!showProjects);
+  const handleTabClick = (tab) => {
+    if (tab !== selectedTab) {
+      setFade("fade-out");
+      setTimeout(() => {
+        setSelectedTab(tab);
+        setFade("fade-in");
+      }, 400);
+    }
   };
+
+  useEffect(() => {
+    setFade("fade-in");
+  }, [selectedTab]);
 
   return (
     <div>
       <header>
         <h1>colin henderson</h1>
-        <div className="button-container">
-          <a href="https://github.com/0xcolinhenderson/" target="_blank">
-            <button className="icon-button">
-              <img src="/github.svg" alt="github-icon" />
-            </button>
+        <div className="header-icons">
+          <a className="linkButton" href="https://github.com/0xcolinhenderson/" target="_blank">
+            <img src="/portfolio/github.svg" alt="github-icon" />
           </a>
-          <button className="icon-button">
-            <a href="https://linkedin.com/in/colinchenderson/" target="_blank">
-              <img src="/linkedin.svg" alt="linkedin-icon" />
-            </a>
-          </button>
-          <a href="mailto:colin.ch.henderson@gmail.com" target="_blank">
-            <button className="icon-button">
-              <img src="/email.svg" alt="email-icon" />
-            </button>
+          <a className="linkButton" href="https://linkedin.com/in/colinchenderson/" target="_blank">
+            <img src="/portfolio/linkedin.svg" alt="linkedin-icon" />
           </a>
-          <button className="location-button">
-            <img src="/location.svg" alt="location-icon" />
-          </button>
-          <p className="location-text">santa cruz, ca</p>
+          <a className="linkButton" href="mailto:colin.ch.henderson@gmail.com" target="_blank">
+            <img src="/portfolio/email.svg" alt="email-icon" />
+          </a>
+          <img id="location" src="/portfolio/location.svg" alt="location-icon" />
+          <p id="location-text">santa cruz, ca</p>
         </div>
-        <h4>[ last updated: 2/07/25 0:25:09 ]</h4>
+        <h4>[ last updated: 2/10/25 23:19:19 ]</h4>
       </header>
       <div id="content">
-        <section id="text">
-          <h2 className="maintext">
+        <section id="intro">
+          <h2 className="title">
             <Typewriter
               options={{
                 autoStart: true,
@@ -57,44 +52,78 @@ const App = () => {
               onInit={(typewriter) => {
                 typewriter
                   .typeString('<span style="font-weight: normal; color: var(--main-color);">hi!</span>')
-                  .pauseFor(2000) 
+                  .pauseFor(2000)
                   .typeString(' i\'m colin henderson.')
                   .start();
               }}
             />
           </h2>
-          <p align="left">
-            I'm currently a third year student at <a target="_blank" href="https://engineering.ucsc.edu/">UC Santa Cruz</a> studying Computer Science. I mainly
-            focus on building web applications, with an emphasis on Python and React. {<br />}{<br />}
-            Outside of programming, I really love writing, recording, and producing music. 
-            If you're interested in getting a track produced, mixed, or mastered, please reach out to me <a href="mailto:colin.ch.henderson@gmail.com">here</a>.
-          </p>
         </section>
-        <section id="text">
-          <h3 className="textheader">
-            <button className="textheader toggle-button hover-grow" onClick={toggleProjects}>
-              projects&nbsp;<img src={showProjects ? "/down.svg" : "/up.svg"} alt="toggle-icon" />
-            </button>
-          </h3>
-        </section>
-        <section id="projects" className={showProjects ? "show" : ""}>
-          <Carousel items={items} />
-        </section>
-        <section id="text">
-          <button className="textheader toggle-button hover-grow" style={{ paddingBottom: '2rem' }} onClick={void 0}>
-            coursework&nbsp;<img src={dummy ? "/down.svg" : "/up.svg"} alt="toggle-icon" />
-          </button>
-        </section>
-        <section id="text">
-          <button className="textheader toggle-button hover-grow" style={{ paddingBottom: '2rem' }} onClick={void 0}>
-            contact&nbsp;<img src={dummy ? "/down.svg" : "/up.svg"} alt="toggle-icon" />
-          </button>
-        </section>
-        <section id="text">
-          <button className="textheader toggle-button hover-grow" style={{ paddingBottom: '2rem' }} onClick={void 0}>
-            resume&nbsp;<img src={"/download.svg"} alt="toggle-icon" />
-          </button>
-        </section>
+        <div className="menu-container">
+          <div className="menu">
+            <section id="intro" onClick={() => handleTabClick("intro")}>
+              <h3 className={`sectionHeader hover-effect ${selectedTab === "intro" ? "active" : ""}`}>
+                intro
+              </h3>
+            </section>
+            <section id="projects" onClick={() => handleTabClick("projects")}>
+              <h3 className={`sectionHeader hover-effect ${selectedTab === "projects" ? "active" : ""}`}>
+                projects
+              </h3>
+            </section>
+            <section id="coursework" onClick={() => handleTabClick("coursework")}>
+              <h3 className={`sectionHeader hover-effect ${selectedTab === "coursework" ? "active" : ""}`}>
+                coursework
+              </h3>
+            </section>
+            <section id="contact" onClick={() => handleTabClick("contact")}>
+              <h3 className={`sectionHeader hover-effect ${selectedTab === "contact" ? "active" : ""}`}>
+                contact
+              </h3>
+            </section>
+            <section id="resume" onClick={() => handleTabClick("resume")}>
+              <h3 className={`sectionHeader hover-effect ${selectedTab === "resume" ? "active" : ""}`}>
+                <a className="resumeDownload hover-effect" href="/">
+                  resume
+                </a>
+              </h3>
+            </section>
+          </div>
+          <div className={`displayedContent ${fade}`}>
+            {selectedTab === "intro" && (
+              <>
+                <p>
+                  I'm currently a third year student at <a className="link" target="_blank" href="https://engineering.ucsc.edu/">UC Santa Cruz</a> studying Computer Science. I mainly
+                  focus on building web applications, with an emphasis on Python and React.
+                </p>
+                <p>
+                  Outside of programming, I really love writing, recording, and producing music.
+                  If you're interested in getting a track produced, mixed, or mastered, please reach out to me <a className="link" href="mailto:colin.ch.henderson@gmail.com">here</a>.
+                </p>
+              </>
+            )}
+            {selectedTab === "projects" && (
+              <>
+                <p>Projects</p>
+              </>
+            )}
+            {selectedTab === "coursework" && (
+              <>
+                <p>Coursework</p>
+              </>
+            )}
+            {selectedTab === "contact" && (
+              <>
+                <p>Contact</p>
+              </>
+            )}
+            {selectedTab === "resume" && (
+              <>
+                <p>Resume</p>
+              </>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
